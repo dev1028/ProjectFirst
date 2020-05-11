6bea3ae59db2bbbbbd9a100b93956326fb188e9b
admin <sm121913@gmail.com>
2020-05-11 12:50:39 +0900

    add

--- ProjectFirst/src/com/yedam/classes/statics/AccountExample.java
+++ ProjectFirst/src/com/yedam/classes/statics/AccountExample.java
@@ -8,7 +8,7 @@ public class AccountExample {
 		Account[] accounts = new Account[10];
 
 		int idx = 0;
-		while (true) {
+		while (true) { 
 			System.out.println("-----------------------------------------");
 			System.out.println("1.계좌생성 | 2.계좌목록  | 3.예금 | 4.출금 | 5.종료");
 			System.out.println("-----------------------------------------");
